9b1e9cd933649e3289c0e57d4b2cfb6931444571
root <root@jdev>
2018-02-28 06:21:45 +0000

    Modified comments

--- object_mover.hpp
+++ object_mover.hpp
@@ -17,10 +17,10 @@ public:
   /**
    * Constructor
    *
-   * @param cluster Ceph cluster 
-   * @param io_ctx_storage IoCtx associated with Storage Pool
-   * @param io_ctx_archive IoCtx associated with Archive Pool
-   * @param thread_pool_size size of thread pool that do asynchronous I/Os
+   * @param[in] cluster Ceph cluster
+   * @param[in] io_ctx_storage IoCtx associated with Storage Pool
+   * @param[in] io_ctx_archive IoCtx associated with Archive Pool
+   * @param[in] thread_pool_size numbuer of threads that execute asynchronous I/Os
    */
   ObjectMover(librados::Rados *cluster,
 	      librados::IoCtx *io_ctx_storage,
@@ -30,9 +30,11 @@ public:
   /**
    * Create an object in the specified tier
    *
-   * @param tier the tier in which to create the object
-   * @param object_name the name of the object
-   * @param value the data of the object
+   * @param[in] tier the tier in which to create the object
+   * @param[in] object_name the name of the object
+   * @param[in] value the data of the object
+   * @param[out] err  0 success
+   * @param[out] err <0 failure
    */
   void Create(Tier tier, const std::string &object_name, const librados::bufferlist &bl, int *err);
   void CreateAsync(Tier tier, const std::string &object_name, const librados::bufferlist &bl, int *err) {
@@ -42,8 +44,10 @@ public:
   /**
    * Move an object to the specified tier
    *
-   * @param tier the tier to which to move the object
-   * @param object_name the name of the object
+   * @param[in] tier the tier to which to move the object
+   * @param[in] object_name the name of the object
+   * @param[out] err  0 success
+   * @param[out] err <0 failure
    */
   void Move(Tier tier, const std::string &object_name, int *err);
   void MoveAsync(Tier tier, const std::string &object_name, int *err) {
@@ -53,7 +57,9 @@ public:
   /**
    * Delete an object
    *
-   * @param object_name the name of the object
+   * @param[in] object_name the name of the object
+   * @param[out] err  0 success
+   * @param[out] err <0 failure
    */
   void Delete(const std::string &object_name, int *err);
   void DeleteAsync(const std::string &object_name, int *err) {
@@ -65,6 +71,8 @@ public:
    * Get the Location of the object
    *
    * @param object_name the name of the object
+   * @retval err  0 success
+   * @retval err <0 failure
    */
   int GetLocation(const std::string &object_name);
 private:
