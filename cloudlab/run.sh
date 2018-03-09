e40e289364d5290a566045cdaf2632f2d8600ea0
kiizawa <kiizawa@node-0.kiizawa-qv34078.cephtiering-pg0.wisc.cloudlab.us>
2018-03-08 19:58:52 -0600

    Setup overlay network

--- cloudlab/run.sh
+++ cloudlab/run.sh
@@ -4,18 +4,17 @@
 
 sudo apt-get update
 
-MASTER_CLOUDLAB_NODE="c220g2-011027.wisc.cloudlab.us"
-SLAVE_NODE_IDS="011005 011002 030632 030631 011130 011126"
-SLAVE_CLOUDLAB_NODES=""
-for NODE_ID in $SLAVE_NODE_IDS
+MASTER_NODE="node-0"
+SLAVE_NODES="node-1 node-2 node-3 node-4 node-5 node-6"
+
+for SLAVE_NODE in $SLAVE_NODES
 do
-    NODE="c220g2-"$NODE_ID".wisc.cloudlab.us"
-    SLAVE_CLOUDLAB_NODES=$SLAVE_CLOUDLAB_NODES" "$NODE
+    ssh $SLAVE_NODE sudo sh -c 'hostname $SLAVE_NODE'
 done
 
 # Setup NFS
 
-# 1. master
+## 1. master
 
 sudo apt-get install -y nfs-kernel-server
 if [ ! -e /opt/nfs ]
@@ -26,12 +25,20 @@ then
     sudo service nfs-kernel-server start
 fi
 
-# 2. slave
+## 2. slave
 
-for SLAVE_NODE in $SLAVE_CLOUDLAB_NODES
+for SLAVE_NODE in $SLAVE_NODES
 do
     ssh $SLAVE_NODE "sudo apt-get install -y nfs-common"
     ssh $SLAVE_NODE "if [ ! -e /tmp/share ]; then mkdir /tmp/share; fi"
     ssh $SLAVE_NODE "sudo mount -t nfs node-0:/opt/nfs /tmp/share"
     ssh $SLAVE_NODE "sudo chmod a+w /tmp/share"
+done
+
+# Setup etcd
+
+for SLAVE_NODE in $SLAVE_NODES
+do
+    scp setup_overlay_network.sh $SLAVE_NODE:/tmp
+    ssh $SLAVE_NODE sudo sh -c './tmp/setup_overlay_network.sh'
 done
---
+++ cloudlab/setup_overlay_network.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ `hostname` == "node-1" ]
+then
+    cd /tmp
+    curl -L  https://github.com/coreos/etcd/releases/download/v2.3.4/etcd-v2.3.4-linux-amd64.tar.gz -o etcd-v2.3.4-\linux-amd64.tar.gz
+    tar xzvf etcd-v2.3.4-linux-amd64.tar.gz
+    cd etcd-v2.3.4-linux-amd64
+    ./etcd -listen-client-urls "http://10.10.1.2:2379" -advertise-client-urls "http://10.10.1.2:2379" &
+fi
+
+IFs="eth0 eth1 eth2 eth3"
+
+for IF in $IFs
+do
+    IP=`echo $(ip --oneline --family inet address show dev $IF) | cut -d ' ' -f 4 | cut -d '.' -f 1-3`
+    if [ -z "$IP" ]
+    then
+	continue
+    fi
+    if [ $IP = "10.10.1" ]
+    then
+	CEPH_IF=$IF
+    fi
+done
+
+sudo sh -c 'echo DOCKER_OPTS=\"--cluster-store=etcd://10.10.1.2:2379 --cluster-advertise=$CEPH_IF:2376\" >> /etc/default/docker'
+sudo service docker restart
