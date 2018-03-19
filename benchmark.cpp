876ed198066e686c82552bf10adbfd53cdd1181d
Ken Iizawa <kiizawa@ucsc.edu>
2018-03-19 20:24:20 +0000

    Refactoring

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,14 +1,41 @@
+#include <stdlib.h>
+#include <unistd.h>
+
 #include <iostream>
 #include <sstream>
 #include <string>
+
 #include <rados/librados.hpp>
+
 #include "object_mover.hpp"
 
-#define PARALLELISM 128
 #define OBJECT_NUM 10000
+#define THREAD_NUM 128
 #define WAIT_MSEC 10
 
-int main() {
+int main(int argc, char *argv[]) {
+
+  int object_num = OBJECT_NUM;
+  int thread_num = THREAD_NUM;
+
+  int opt;
+
+  while ((opt = ::getopt(argc, argv, "n:t:h")) != -1) {
+    switch (opt) {
+    case 'n':
+      object_num = ::atoi(optarg);
+      break;
+
+    case 't':
+      thread_num = ::atoi(optarg);
+      break;
+
+    case 'h':
+    default:
+      printf("Usage: %s [-n objects] [-t threads]\n", argv[0]);
+      break;
+    }
+  }
 
   int ret = 0;
 
@@ -16,10 +43,9 @@ int main() {
 
   /**
    * Operations on obejcts can be executed asynchronously by background threads.
-   * thread_pool_size controls the parallelism.
+   * thread_num controls the parallelism.
    */
-  int thread_pool_size = PARALLELISM;
-  ObjectMover om(thread_pool_size);
+  ObjectMover om(thread_num);
 
   /* Create an object in Fast Tier (SSD) */
   librados::bufferlist bl;
@@ -27,17 +53,17 @@ int main() {
     bl.append("ceph1234");
   }
 
-  int rets[PARALLELISM];
-  for (int i = 0; i < PARALLELISM; i++) {
-    rets[i] = 0;
+  std::vector<int> rets;
+  for (int i = 0; i < thread_num; i++) {
+    rets.push_back(0);
   }
-  for (int i = 0; i < OBJECT_NUM; i++) {
+  for (int i = 0; i < object_num; i++) {
     std::ostringstream os;
     os << std::setfill('0') << std::setw(10) << i;
     std::string object = os.str();
   retry:
     int used = 0;
-    for (int j = 0; j < PARALLELISM; j++) {
+    for (int j = 0; j < thread_num; j++) {
       int ret = rets[j];
       if (ret == 0) {
 	rets[j] = 1;
@@ -50,20 +76,20 @@ int main() {
 	assert(ret == 1);
       }
     }
-    if (used == PARALLELISM) {
+    if (used == thread_num) {
       usleep(WAIT_MSEC*1000);
       goto retry;
     }
   }
   while (true) {
     int done = 0;
-    for (int j = 0; j < PARALLELISM; j++) {
+    for (int j = 0; j < thread_num; j++) {
       int ret = rets[j];
       if (ret == 0) {
 	done++;
       }
     }
-    if (done == PARALLELISM) {
+    if (done == thread_num) {
       printf("all writes done!\n");
       break;
     }
