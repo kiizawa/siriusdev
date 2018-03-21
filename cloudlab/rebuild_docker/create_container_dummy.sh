0a5ca0f1632e6db362f93d64fc7e8ed65c363d62
Ken Iizawa <kiizawa@ucsc.edu>
2018-03-21 17:23:25 -0500

    Added scripts

---
+++ cloudlab/rebuild_docker/create_container_dummy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+HOST=`hostname`
+
+DOCKER_IMAGE=kiizawa/siriusdev:base4
+
+function start() {
+    docker run -it -d --privileged  \
+	--name $HOST_NAME \
+	$DOCKER_IMAGE /bin/bash
+}
+
+HOST_NAME=$HOST"-docker"
+
+start
