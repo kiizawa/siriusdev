a162809c2051d8a8943d9d2a01e34391d8cf7efc
Ken Iizawa <kiizawa@ucsc.edu>
2018-03-25 20:58:33 +0000

    Refactoring

--- benchmark.cpp
+++ benchmark.cpp
@@ -13,45 +13,85 @@
 #define THREAD_NUM 128
 #define WAIT_MSEC 10
 
-int main(int argc, char *argv[]) {
+enum Mode {
+  READ,
+  WRITE,
+  MOVE
+};
 
-  int object_num = OBJECT_NUM;
-  int thread_num = THREAD_NUM;
+void read(int thread_num, int object_num) {
 
-  ObjectMover::Tier slow_tier = ObjectMover::SLOW;
-  ObjectMover::Tier fast_tier = ObjectMover::FAST;
+  /* Initialize a Object Mover */
 
-  int opt;
+  /**
+   * Operations on obejcts can be executed asynchronously by background threads.
+   * thread_num controls the parallelism.
+   */
+  ObjectMover om(thread_num);
+  
+  /* Read objects from Slow Tier (HDD) */
 
-  while ((opt = ::getopt(argc, argv, "n:t:mph")) != -1) {
-    switch (opt) {
-    case 'n':
-      object_num = ::atoi(optarg);
-      break;
+  std::vector<int> rets;
+  for (int i = 0; i < thread_num; i++) {
+    rets.push_back(1);
+  }
 
-    case 't':
-      thread_num = ::atoi(optarg);
-      break;
+  /* read */
 
-    case 'm':
-      slow_tier = ObjectMover::SLOW;
-      fast_tier = ObjectMover::FAST;
-      break;
+  std::vector<librados::bufferlist*> bls;
+  for (int i = 0; i < thread_num; i++) {
+    bls.push_back(new librados::bufferlist);
+  }
 
-    case 'p':
-      slow_tier = ObjectMover::ARCHIVE;
-      fast_tier = ObjectMover::SLOW;
-      break;
+  for (int i = 0; i < object_num; i++) {
+    std::ostringstream os;
+    os << "oname" << std::setfill('0') << std::setw(10) << i;
+    std::string object = os.str();
+  retry:
+    int used = 0;
+    for (int j = 0; j < thread_num; j++) {
+      int ret = rets[j];
+      if (ret > 0) {
+	rets[j] = 0;
+	bls[j]->clear();
+	om.ReadAsync(object, bls[j], &rets[j], false);
+	// while (rets[j] == 1);
+	// assert(rets[j] == 0);
+	break;
+      } else {
+	used++;
+	assert(ret == 0);
+      }
+    }
+    if (used == thread_num) {
+      usleep(WAIT_MSEC*1000);
+      goto retry;
+    }
+  }
 
-    case 'h':
-    default:
-      printf("Usage: %s [-n objects] [-t threads]\n", argv[0]);
-      exit(0);
+  /* wait */
+
+  while (true) {
+    int done = 0;
+    for (int j = 0; j < thread_num; j++) {
+      int ret = rets[j];
+      if (ret > 0) {
+	done++;
+      }
+    }
+    if (done == thread_num) {
+      printf("all reads (slow) done!\n");
       break;
     }
   }
 
-  int ret = 0;
+  std::vector<librados::bufferlist*>::iterator it;
+  for (it = bls.begin(); it != bls.end(); it++) {
+    delete *it;
+  }
+}
+
+void write(ObjectMover::Tier tier, int thread_num, int object_num) {
 
   /* Initialize a Object Mover */
 
@@ -62,27 +102,30 @@ int main(int argc, char *argv[]) {
   ObjectMover om(thread_num);
 
   /* Create objects in Slow Tier (HDD) */
-  librados::bufferlist bl;
-  for (int i = 0; i < 1024*1024; i++) {
-    bl.append("ceph1234");
-  }
-  int object_size = bl.length();
 
   std::vector<int> rets;
   for (int i = 0; i < thread_num; i++) {
     rets.push_back(0);
   }
+
+  /* write */
+
+  librados::bufferlist bl;
+  for (int i = 0; i < 1024*1024; i++) {
+    bl.append("ceph1234");
+  }
+  
   for (int i = 0; i < object_num; i++) {
     std::ostringstream os;
     os << "oname" << std::setfill('0') << std::setw(10) << i;
     std::string object = os.str();
-  retry1:
+  retry:
     int used = 0;
     for (int j = 0; j < thread_num; j++) {
       int ret = rets[j];
       if (ret == 0) {
 	rets[j] = 1;
-	om.CreateAsync(slow_tier, object, bl, &rets[j]);
+	om.CreateAsync(tier, object, bl, &rets[j]);
 	// while (rets[j] == 1);
 	// assert(rets[j] == 0);
 	break;
@@ -93,9 +136,12 @@ int main(int argc, char *argv[]) {
     }
     if (used == thread_num) {
       usleep(WAIT_MSEC*1000);
-      goto retry1;
+      goto retry;
     }
   }
+
+  /* wait */
+
   while (true) {
     int done = 0;
     for (int j = 0; j < thread_num; j++) {
@@ -110,73 +156,36 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  std::vector<librados::bufferlist*> bls;
-  for (int i = 0; i < thread_num; i++) {
-    bls.push_back(new librados::bufferlist);
-  }
+}
 
-  /* Read objects from Slow Tier (HDD) */
+void move(ObjectMover::Tier tier, int thread_num, int object_num) {
 
-  for (std::vector<int>::iterator it = rets.begin(); it != rets.end() ; it++) {
-    *it = object_size;
-  }
+  /* Initialize a Object Mover */
 
-  for (int i = 0; i < object_num; i++) {
-    std::ostringstream os;
-    os << "oname" << std::setfill('0') << std::setw(10) << i;
-    std::string object = os.str();
-  retry2:
-    int used = 0;
-    for (int j = 0; j < thread_num; j++) {
-      int ret = rets[j];
-      if (ret == object_size) {
-	rets[j] = 1;
-	bls[j]->clear();
-	om.ReadAsync(object, bls[j], &rets[j], false);
-	// while (rets[j] == 1);
-	// assert(rets[j] == 0);
-	break;
-      } else {
-	used++;
-	assert(ret == 1);
-      }
-    }
-    if (used == thread_num) {
-      usleep(WAIT_MSEC*1000);
-      goto retry2;
-    }
-  }
-  while (true) {
-    int done = 0;
-    for (int j = 0; j < thread_num; j++) {
-      int ret = rets[j];
-      if (ret == object_size) {
-	done++;
-      }
-    }
-    if (done == thread_num) {
-      printf("all reads (slow) done!\n");
-      break;
-    }
-  }
+  /**
+   * Operations on obejcts can be executed asynchronously by background threads.
+   * thread_num controls the parallelism.
+   */
+  ObjectMover om(thread_num);
 
   /* Move objects into Fast Tier (SSD) */
 
-  for (std::vector<int>::iterator it = rets.begin(); it != rets.end() ; it++) {
-    *it = 0;
+  std::vector<int> rets;
+  for (int i = 0; i < thread_num; i++) {
+    rets.push_back(0);
   }
 
   for (int i = 0; i < object_num; i++) {
     std::ostringstream os;
     os << "oname" << std::setfill('0') << std::setw(10) << i;
     std::string object = os.str();
-  retry3:
+  retry:
     int used = 0;
     for (int j = 0; j < thread_num; j++) {
       int ret = rets[j];
       if (ret == 0) {
 	rets[j] = 1;
-	om.MoveAsync(fast_tier, object, &rets[j]);
+	om.MoveAsync(tier, object, &rets[j]);
 	// while (rets[j] == 1);
 	// assert(rets[j] == 0);
 	break;
@@ -187,7 +196,7 @@ int main(int argc, char *argv[]) {
     }
     if (used == thread_num) {
       usleep(WAIT_MSEC*1000);
-      goto retry3;
+      goto retry;
     }
   }
   while (true) {
@@ -203,50 +212,87 @@ int main(int argc, char *argv[]) {
       break;
     }
   }
+}
 
-  /* Read objects from Fast Tier (SSD) */
+int main(int argc, char *argv[]) {
 
-  for (std::vector<int>::iterator it = rets.begin(); it != rets.end() ; it++) {
-    *it = object_size;
-  }
+  std::string prefix;
+  Mode mode;
+  int object_num = OBJECT_NUM;
+  int thread_num = THREAD_NUM;
+  ObjectMover::Tier tier;
 
-  for (int i = 0; i < object_num; i++) {
-    std::ostringstream os;
-    os << "oname" << std::setfill('0') << std::setw(10) << i;
-    std::string object = os.str();
-  retry4:
-    int used = 0;
-    for (int j = 0; j < thread_num; j++) {
-      int ret = rets[j];
-      if (ret == object_size) {
-	rets[j] = 1;
-	bls[j]->clear();
-	om.ReadAsync(object, bls[j], &rets[j], true);
-	// while (rets[j] == 1);
-	// assert(rets[j] == 0);
-	break;
+  int opt;
+  while ((opt = ::getopt(argc, argv, "x:o:t:m:r:h")) != -1) {
+    switch (opt) {
+
+    case 'x':
+      /* prefix */
+      prefix = optarg;
+      break;
+
+    case 'm':
+      /* access mode */
+      if (*optarg == 'r') {
+        mode = READ;
+      } else if (*optarg == 'w') {
+	mode = WRITE;
+      } else if (*optarg == 'm') {
+	mode = MOVE;
       } else {
-	used++;
-	assert(ret == 1);
+	printf("Invalid access mode\n");
+	exit(0);
       }
-    }
-    if (used == thread_num) {
-      usleep(WAIT_MSEC*1000);
-      goto retry4;
-    }
-  }
-  while (true) {
-    int done = 0;
-    for (int j = 0; j < thread_num; j++) {
-      int ret = rets[j];
-      if (ret == object_size) {
-	done++;
+      break;
+
+    case 'o':
+      /* number of objects */
+      object_num = ::atoi(optarg);
+      break;
+
+    case 't':
+      /* number of threads */
+      thread_num = ::atoi(optarg);
+      break;
+
+    case 'r':
+      /* tier */
+      if (*optarg == 'a') {
+	tier = ObjectMover::ARCHIVE;
+      } else if (*optarg == 's') {
+	tier = ObjectMover::SLOW;
+      } else if (*optarg == 'f') {
+	tier = ObjectMover::FAST;
+      } else {
+	printf("Invalid tier\n");
+	exit(0);
       }
-    }
-    if (done == thread_num) {
-      printf("all reads (fast) done!\n");
+      break;
+
+    case 'h':
+    default:
+      printf("Usage: %s [-n objects] [-t threads]\n", argv[0]);
+      exit(0);
       break;
     }
+  }
+
+  switch(mode) {
+
+  case READ:
+    read(thread_num, object_num);
+    break;
+
+  case WRITE:
+    write(tier, thread_num, object_num);
+    break;
+
+  case MOVE:
+    move(tier, thread_num, object_num);
+    break;
+
+  default:
+    break;    
   }
   
   return 0;
