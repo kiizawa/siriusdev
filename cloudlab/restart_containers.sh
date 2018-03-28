517d3717a65c940de7c9c90142c7504e621595e2
Ken Iizawa <kiizawa@ucsc.edu>
2018-03-28 10:48:28 -0500

    Refactoring

--- cloudlab/restart_containers.sh
+++ cloudlab/restart_containers.sh
@@ -3,15 +3,19 @@
 set -x
 
 CLIENTS="node-0 node-1"
-SERVERS="node-5 node-6 node-7 node-8"
+SERVERS="node-2 node-3 node-4 node-5"
 ALL_NODES=$CLIENTS" "$SERVERS
 
 for NODE in $ALL_NODES
 do
-    #scp stop_and_remove_container.sh $NODE:/tmp
-    #ssh $NODE sh -c "/tmp/stop_and_remove_container.sh"
-    #scp create_container.sh $NODE:/tmp
-    #ssh $NODE sh -c "/tmp/create_container.sh"
-    scp create_container_pool_based.sh $NODE:/tmp
-    ssh $NODE sh -c "/tmp/create_container_pool_based.sh"
+    scp stop_and_remove_container.sh $NODE:/tmp
+    ssh $NODE sh -c "/tmp/stop_and_remove_container.sh"
+done
+
+for NODE in $ALL_NODES
+do
+    scp create_container.sh $NODE:/tmp
+    ssh $NODE sh -c "/tmp/create_container.sh"
+    #scp create_container_pool_based.sh $NODE:/tmp
+    #ssh $NODE sh -c "/tmp/create_container_pool_based.sh"
 done
