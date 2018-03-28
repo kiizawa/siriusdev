38ede23f99301ec013e94ab39ec0c3d35f134122
Ken Iizawa <kiizawa@ucsc.edu>
2018-03-27 19:40:25 -0500

    Refactoring

--- cloudlab/setup_overlay_network.sh
+++ cloudlab/setup_overlay_network.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-if [ `hostname` = "node-0" ]
+MASTER_NODE="node-0"
+
+if [ `hostname` = $MASTER_NODE ]
 then
     cd /tmp
     curl -L  https://github.com/coreos/etcd/releases/download/v2.3.4/etcd-v2.3.4-linux-amd64.tar.gz -o etcd-v2.3.4-\linux-amd64.tar.gz
@@ -32,7 +34,7 @@ sudo ifconfig $CEPH_IF mtu 9000
 sudo env CEPH_IF=$CEPH_IF sh -c 'echo DOCKER_OPTS=\"--cluster-store=etcd://10.10.1.1:2379 --cluster-advertise=$CEPH_IF:2376 --mtu=9000\" >> /etc/default/docker'
 sudo service docker restart
 
-if [ `hostname` = "node-0" ]
+if [ `hostname` = $MASTER_NODE ]
 then
     docker network create --opt com.docker.network.driver.mtu=9000 -d overlay --subnet=192.168.0.0/16 cephnet
 fi
