1e68d58d2b5d2d68816ec0e1f9a10924e568cba6
Ken Iizawa <kiizawa@ucsc.edu>
2018-04-01 06:23:52 +0000

    Added debug script

---
+++ debug.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OBJ=$1
+
+for i in `seq 12 23`
+do
+    node=192.168.0.$i
+    ssh $node hostname
+    ssh $node cat /dev/shm/osd*log | grep -e get_onode -e osd_op_reply | grep $OBJ
+done
