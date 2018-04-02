ccc5904f895ee7212fe98b0a01d9efbbf5d00c08
Ken Iizawa <kiizawa@ucsc.edu>
2018-04-02 16:30:49 -0500

    Added script that converts docker image to singularity image

---
+++ singularity/convert_from_docker_to_singularity.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+WORK_DIR=/dev/shm
+
+cd $WORK_DIR
+mkdir cache
+mkdir tmp
+mkdir localcache
+
+export SINGULARITY_CACHEDIR=$WORK_DIR/cache
+export SINGULARITY_TMPDIR=$WORK_DIR/tmp 
+export SINGULARITY_LOCALCACHEDIR=$WORK_DIR/localcache
+
+singularity create siriusdev.img
+singularity build siriusdev.img docker://kiizawa/siriusdev
