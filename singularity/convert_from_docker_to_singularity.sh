694a6287cda10a07b198222b4cc89d30d581521a
kiizawa <kiizawa@node-0.kiizawa-qv35107.cephtiering-pg0.wisc.cloudlab.us>
2018-04-02 17:42:36 -0500

    Refactoring

--- singularity/convert_from_docker_to_singularity.sh
+++ singularity/convert_from_docker_to_singularity.sh
@@ -2,17 +2,15 @@
 
 WORK_DIR=/dev/shm
 DOCKER_IMAGE=kiizawa/siriusdev
-HOME=/home/$USER
+SINGULARITY_IMAGE=/dev/shm/siriusdev.img
 
-cd $WORK_DIR
-mkdir cache
-mkdir tmp
-mkdir localcache
-
-export HOME
 export SINGULARITY_CACHEDIR=$WORK_DIR/cache
 export SINGULARITY_TMPDIR=$WORK_DIR/tmp 
 export SINGULARITY_LOCALCACHEDIR=$WORK_DIR/localcache
 
-singularity create siriusdev.img
-singularity build siriusdev.img docker://$DOCKER_IMAGE
+rm -rf $SINGULARITY_CACHEDIR; mkdir $SINGULARITY_CACHEDIR
+rm -rf $SINGULARITY_TMPDIR; mkdir $SINGULARITY_TMPDIR
+rm -rf $SINGULARITY_LOCALCACHEDIR; mkdir $SINGULARITY_LOCALCACHEDIR
+
+singularity create $SINGULARITY_IMAGE
+singularity build $SINGULARITY_IMAGE docker://$DOCKER_IMAGE
