26382ee76eca8d1245f27c8141c7657ab5241804
kiizawa <kiizawa@node-0.kiizawa-qv35107.cephtiering-pg0.wisc.cloudlab.us>
2018-04-02 17:06:16 -0500

    Added environment variable

--- singularity/convert_from_docker_to_singularity.sh
+++ singularity/convert_from_docker_to_singularity.sh
@@ -2,12 +2,14 @@
 
 WORK_DIR=/dev/shm
 DOCKER_IMAGE=kiizawa/siriusdev
+HOME=/home/$USER
 
 cd $WORK_DIR
 mkdir cache
 mkdir tmp
 mkdir localcache
 
+export $HOME
 export SINGULARITY_CACHEDIR=$WORK_DIR/cache
 export SINGULARITY_TMPDIR=$WORK_DIR/tmp 
 export SINGULARITY_LOCALCACHEDIR=$WORK_DIR/localcache
