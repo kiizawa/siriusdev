da9624e7f04f6ee729e776ed31dfb40206b291fa
Ken Iizawa <kiizawa@ucsc.edu>
2018-04-09 13:01:06 -0500

    Added script

---
+++ singularity/create_singularity_container_pool_based.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+set -ex
+
+SINGULARITY_IMAGE=/dev/shm/siriusdev.img
+HOST=`hostname`
+CEPH_NET=10.10.1.0/16
+CEPH_CONF_DIR=/tmp/share
+
+DUMMY_HOME_DIR_SSD=/tmp/kiizawa.ssd
+if [ ! -e $DUMMY_HOME_DIR_SSD ]
+then
+    mkdir $DUMMY_HOME_DIR_SSD
+fi
+
+DUMMY_HOME_DIR_HDD=/tmp/kiizawa.hdd
+if [ ! -e $DUMMY_HOME_DIR_HDD ]
+then
+    mkdir $DUMMY_HOME_DIR_HDD
+fi
+
+CEPH_DIR_SSD=/tmp/ceph.ssd
+if [ ! -e $CEPH_DIR_SSD ]
+then
+    mkdir $CEPH_DIR_SSD
+fi
+
+CEPH_DIR_HDD=/tmp/ceph.hdd
+if [ ! -e $CEPH_DIR_HDD ]
+then
+    mkdir $CEPH_DIR_HDD
+fi
+
+CLIENTS="node-0 node-1"
+SERVERS="node-2 node-3 node-4 node-5"
+
+function start() {
+
+    singularity instance.start \
+	--writable \
+        -H $DUMMY_HOME_DIR \
+	-B $CEPH_CONF_DIR:/ceph_conf \
+	-B $CEPH_DIR:/ceph \
+	$SINGULARITY_IMAGE siriusdev
+
+    SINGULARITYENV_RUN_MON=$RUN_MON \
+    SINGULARITYENV_RUN_OSD=$RUN_OSD \
+    SINGULARITYENV_CEPH_CONF_DIR=/ceph_conf \
+    SINGULARITYENV_CEPH_DIR=/ceph \
+    SINGULARITYENV_POOL_SIZE=1 \
+    SINGULARITYENV_PG_NUM=$PG_NUM \
+    SINGULARITYENV_OP_THREADS=32 \
+    SINGULARITYENV_OSD_TYPE=$OSD_TYPE \
+    SINGULARITYENV_POOL=$POOL \
+    SINGULARITYENV_CEPH_PUBLIC_NETWORK=$CEPH_NET \
+    SINGULARITYENV_HOSTNAME=$HOSTNAME \
+    $DEVICE_ARGS \
+        singularity run --writable instance://siriusdev /root/start_ceph.sh
+
+    singularity exec instance://siriusdev \
+	bash -c 'LD_LIBRARY_PATH=/usr/local/lib ceph -s -c /ceph_conf/ceph.conf'
+}
+
+power2() { echo "x=l($1)/l(2); scale=0; 2^((x+0.5)/1)" | bc -l; }
+
+OSD_NUM_PER_POOL=`echo $SERVERS | wc -w`
+PG_NUM=`expr $OSD_NUM_PER_POOL \* 100`
+PG_NUM=`power2 $PG_NUM`
+
+# clients
+
+#set +e
+#R=`echo $CLIENTS | grep $HOST`
+#set -e
+
+#if [ -n "$R" ]
+#then
+#    HOST_NAME=$HOST"-docker"
+#    HOST_ADDR=`get_client_ip_addr $HOST`
+#    RUN_MON=0
+#    RUN_OSD=0
+#    start
+#    exit
+#fi
+
+# servers
+
+set +e
+R=`echo $SERVERS | grep $HOST`
+set -e
+
+FIRST_SERVER=`echo $SERVERS | cut -d ' ' -f 1`
+
+if [ -n "$R" ]
+then
+
+    HOSTNAME=$HOST"-singularity-ssd"
+    if [ $HOST = $FIRST_SERVER ]
+    then
+	RUN_MON=1
+	RUN_OSD=1
+    else
+	RUN_MON=0
+	RUN_OSD=1
+    fi
+    POOL="cache_pool"
+    OSD_TYPE="bluestore"
+    DUMMY_HOME_DIR=$DUMMY_HOME_DIR_SSD
+    CEPH_DIR=$CEPH_DIR_SSD
+    DEVICE_ARGS="SINGULARITYENV_BS_FAST_CREATE=false SINGULARITYENV_BS_SLOW_BD=/dev/sdc"
+    start
+
+    sleep 5
+
+    HOSTNAME=$HOST"-singularity-hdd"
+    RUN_MON=0
+    RUN_OSD=1
+    POOL="storage_pool"
+    OSD_TYPE="bluestore"
+    DUMMY_HOME_DIR=$DUMMY_HOME_DIR_HDD
+    CEPH_DIR=$CEPH_DIR_HDD
+    DEVICE_ARGS="SINGULARITYENV_BS_FAST_CREATE=false SINGULARITYENV_BS_SLOW_BD=/dev/sdb"
+    start
+fi
