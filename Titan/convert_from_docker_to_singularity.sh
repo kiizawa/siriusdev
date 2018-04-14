700a8e1a1e138749c0e4464f10781a8a77ed733c
Ken Iizawa <kiizawa@ucsc.edu>
2018-04-14 18:49:42 -0500

    Updated

--- Titan/convert_from_docker_to_singularity.sh
+++ Titan/convert_from_docker_to_singularity.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 WORK_DIR=/dev/shm
-DOCKER_IMAGE=kiizawa/siriusdev:memstore_journal_xattr_monip
+DOCKER_IMAGE=kiizawa/siriusdev:memstore_journal_xattr_monip_dummydir4
 SINGULARITY_IMAGE=/dev/shm/siriusdev.img
 SANDBOX_DIR=$WORK_DIR/siriusdev_sandbox_dir
 
@@ -12,6 +12,7 @@ export SINGULARITY_LOCALCACHEDIR=$WORK_DIR/localcache
 rm -rf $SINGULARITY_CACHEDIR; mkdir $SINGULARITY_CACHEDIR
 rm -rf $SINGULARITY_TMPDIR; mkdir $SINGULARITY_TMPDIR
 rm -rf $SINGULARITY_LOCALCACHEDIR; mkdir $SINGULARITY_LOCALCACHEDIR
+rm -rf $SANDBOX_DIR
 
 singularity build --sandbox $SANDBOX_DIR docker://$DOCKER_IMAGE
 singularity build --writable $SINGULARITY_IMAGE $SANDBOX_DIR
