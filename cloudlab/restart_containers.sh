703c8b03556c7e37ddc630edb96fd68a616afbb9
Ken Iizawa <kiizawa@ucsc.edu>
2018-05-10 11:47:37 -0500

    Updated

--- cloudlab/restart_containers.sh
+++ cloudlab/restart_containers.sh
@@ -3,7 +3,7 @@
 set -x
 
 CLIENTS="node-0"
-SERVERS="node-1 node-2"
+SERVERS="node-1 node-2 node-3"
 ALL_NODES=$CLIENTS" "$SERVERS
 
 for NODE in $ALL_NODES
@@ -12,10 +12,14 @@ do
     ssh $NODE sh -c "/tmp/stop_and_remove_container.sh"
 done
 
+exit
+
 for NODE in $ALL_NODES
 do
-    #scp create_container.sh $NODE:/tmp
-    #ssh $NODE sh -c "/tmp/create_container.sh"
-    scp create_container_pool_based.sh $NODE:/tmp
-    ssh $NODE sh -c "/tmp/create_container_pool_based.sh"
+    #scp create_container_micro.sh $NODE:/tmp
+    #ssh $NODE sh -c "/tmp/create_container_micro.sh"
+    #scp create_container_pool_based.sh $NODE:/tmp
+    #ssh $NODE sh -c "/tmp/create_container_pool_based.sh"
+    scp create_container_pool_based_separate_nodes.sh $NODE:/tmp
+    ssh $NODE sh -c "/tmp/create_container_pool_based_separate_nodes.sh"
 done
